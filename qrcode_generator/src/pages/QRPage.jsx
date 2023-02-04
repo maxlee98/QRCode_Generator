6554ccfd242c2cb497cae3964cbeb8b9468e132b
maxlee <max.lee98@gmail.com>
2023-02-04 20:20:46 +0800

    Edited Styling of QRCode
    Added more options to QRCode Styling

--- qrcode_generator/src/pages/QRPage.jsx
+++ qrcode_generator/src/pages/QRPage.jsx
@@ -5,7 +5,10 @@ import "./QRPage.css";
 const QRGenerator = () => {
   const [link, setLink] = useState("");
   const [image, setImage] = useState(null);
-  const [color, setColor] = useState("#000000");
+  const [qrtype, setQrType] = useState("squares");
+  const [fgcolor, setFgColor] = useState("#000000");
+  const [bgcolor, setBgColor] = useState("#ffffff");
+  const [eyecolor, setEyeColor] = useState("#000000");
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -43,18 +46,63 @@ const QRGenerator = () => {
     <div className="qr-generator">
       <h1>Generate QR Code</h1>
       <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Enter Link"
-          value={link}
-          onChange={(e) => setLink(e.target.value)}
-        />
-        <input type="file" accept="image/*" onChange={handleImageUpload} />
-        <input
-          type="color"
-          value={color}
-          onChange={(e) => setColor(e.target.value)}
-        />
+        <div>
+          <label htmlFor="link-input">URL Link:</label>
+          <input
+            id="link-input"
+            type="text"
+            placeholder="Enter Link"
+            value={link}
+            onChange={(e) => setLink(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="image-input">Image Overlay:</label>
+          <input
+            id="image-input"
+            type="file"
+            accept="image/*"
+            onChange={handleImageUpload}
+          />
+        </div>
+        <div>
+          <label htmlFor="selection-input">QR Type:</label>
+          <select
+            id="selection-input"
+            value={qrtype}
+            onChange={(e) => setQrType(e.target.value)}
+          >
+            <option value="squares">Squares</option>
+            <option value="dots">Dots</option>
+          </select>
+        </div>
+        <div>
+          <label htmlFor="eyeColor-input">Eye Colour:</label>
+          <input
+            id="eyeColor-input"
+            type="color"
+            value={eyecolor}
+            onChange={(e) => setEyeColor(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="fg-input">Foreground Colour:</label>
+          <input
+            id="fg-input"
+            type="color"
+            value={fgcolor}
+            onChange={(e) => setFgColor(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="bg-input">Background Colour:</label>
+          <input
+            id="bg-input"
+            type="color"
+            value={bgcolor}
+            onChange={(e) => setBgColor(e.target.value)}
+          />
+        </div>
         <button type="submit">Generate QR Code</button>
       </form>
       {link && (
@@ -62,8 +110,10 @@ const QRGenerator = () => {
           <QRCode
             value={link}
             size={256}
-            bgColor="#FFFFFF"
-            fgColor={color}
+            qrStyle={qrtype}
+            eyeColor={eyecolor}
+            bgColor={bgcolor}
+            fgColor={fgcolor}
             logoImage={image}
             logoWidth={75}
             logoHeight={75}
